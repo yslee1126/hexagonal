35bba65d67df985716c1b801d61fb51587b78f5e
이용성 <lee.yongsung@joongang.co.kr>
2024-12-13 09:54:54 +0900

    first commit

---
+++ src/main/kotlin/groot/hexagonal/HexagonalApplication.kt
@@ -0,0 +1,11 @@
+package groot.hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<HexagonalApplication>(*args)
+}
